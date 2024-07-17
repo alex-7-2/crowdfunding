8aa77d5bcb7d095550b1bb0ea0f35905e91e06f7
sashkajanevska <janevska_sashka@yahoo.com>
2024-07-17 09:54:21 +0200

    header final

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const mobileLayout = document.getElementById("mobileLayoutMode");
+const navBackground = document.getElementById("mobileNavBackground");
 const mobileNav = document.getElementById("mobileNav");
 const toggleButtons = document.querySelectorAll(".toggle");
 
+/* Mobile Nav */
+
 const toggleNav = () => {
   mobileNav.classList.toggle("active");
-  mobileLayout.classList.toggle("active");
+  navBackground.classList.toggle("active");
   toggleButtons.forEach((button) => button.classList.toggle("active"));
 };
 
 toggleButtons.forEach((button) => button.addEventListener("click", toggleNav));
 
-mobileLayout.addEventListener("click", () => {
+navBackground.addEventListener("click", () => {
   if (mobileNav.style.maxHeight !== 0) {
     toggleNav();
   } 
 });
-
-
